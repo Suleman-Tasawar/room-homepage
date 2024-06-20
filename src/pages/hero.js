e25350d984b87e396bd5f9b6f6073fa5e18469fa
Suleman Tasawar <sulemantasawar9@gmail.com>
2024-06-20 13:25:43 +0500

    Update Fixed the slider problem added responsivness

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -27,10 +27,10 @@ const Hero = () => {
             <Button className = 'margin__top__2'/>
         </div>
         <div className='slider'>
-            <button onClick={prevPage}>
+            <button onClick={prevPage} disabled = {pageData=== 0}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
             </button>
-            <button onClick={nextPage}>
+            <button onClick={nextPage} disabled = {pageData >=data.length-1}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg>
             </button>
         </div>
